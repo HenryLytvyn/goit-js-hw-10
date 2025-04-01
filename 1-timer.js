b6b78eae3554e694b771e5469a300820b149fe62
HenryLytvyn <hennadii.lytvyn@gmail.com>
2025-04-01 11:55:26 +0000

    Deploying to gh-pages from @ HenryLytvyn/goit-js-hw-10@78702ac726272f0fae0b5cac5b1ac9fc91f08024 🚀

--- 1-timer.js
+++ 1-timer.js
@@ -1,17 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{f as c,i as u}from"./assets/vendor-BbbuE1sJ.js";const t=document.querySelector("button[data-start]"),s=document.querySelector("#datetime-picker"),p=document.querySelector(".timer");t.disabled=!0;let r,n;c("#datetime-picker",{enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){e[0]>Date.now()?(r=e[0],t.disabled=!1,t.classList.add("start-active")):(u.show({title:"Error",message:"Please choose a date in the future",backgroundColor:"#B51B1B",image:"../img/error.svg",timeout:"3000"}),t.disabled=!0,t.classList.remove("start-active")),r&&(n=r.getTime()-Date.now())}});t.addEventListener("click",m);function m(){s.style.cursor="default",t.classList.remove("start-active");const e=setInterval(()=>{if(n>999){t.disabled=!0,s.disabled=!0,n-=1e3;const a=v(n);p.innerHTML=`<div class="field">
-  <span class="value" data-days>${String(a.days).padStart(2,"0")}</span>
-  <span class="label">Days</span>
-</div>
-<div class="field">
-  <span class="value" data-hours>${String(a.hours).padStart(2,"0")}</span>
-  <span class="label">Hours</span>
-</div>
-<div class="field">
-  <span class="value" data-minutes>${String(a.minutes).padStart(2,"0")}</span>
-  <span class="label">Minutes</span>
-</div>
-<div class="field">
-  <span class="value" data-seconds>${String(a.seconds).padStart(2,"0")}</span>
-  <span class="label">Seconds</span>
-</div>`}else s.disabled=!1,s.style.cursor="pointer",clearInterval(e)},1e3)}function v(e){const o=Math.floor(e/864e5),i=Math.floor(e%864e5/36e5),l=Math.floor(e%864e5%36e5/6e4),d=Math.floor(e%864e5%36e5%6e4/1e3);return{days:o,hours:i,minutes:l,seconds:d}}
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{f as h,i as f}from"./assets/vendor-BbbuE1sJ.js";const e=document.querySelector("button[data-start]"),a=document.querySelector("#datetime-picker");e.disabled=!0;let n,o;h("#datetime-picker",{enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(t){t[0]>Date.now()?(n=t[0],e.disabled=!1,e.classList.add("start-active")):(f.show({title:"Error",message:"Please choose a date in the future",backgroundColor:"#B51B1B",iconUrl:"../img/error.svg",timeout:"3000"}),e.disabled=!0,e.classList.remove("start-active")),n&&(o=n.getTime()-Date.now())}});e.addEventListener("click",y);function y(){a.style.cursor="default",e.classList.remove("start-active");const t=document.querySelector(".value[data-days]"),s=document.querySelector(".value[data-hours]"),d=document.querySelector(".value[data-minutes]"),i=document.querySelector(".value[data-seconds]"),u=setInterval(()=>{if(o>990){e.disabled=!0,a.disabled=!0,o=n.getTime()-Date.now();const r=v(o);t.innerHTML=`${String(r.days).padStart(2,"0")}`,s.innerHTML=`${String(r.hours).padStart(2,"0")}`,d.innerHTML=`${String(r.minutes).padStart(2,"0")}`,i.innerHTML=`${String(r.seconds).padStart(2,"0")}`}else{a.disabled=!1,a.style.cursor="pointer",clearInterval(u);return}},1e3)}function v(t){const r=Math.floor(t/864e5),c=Math.floor(t%864e5/36e5),l=Math.floor(t%864e5%36e5/6e4),m=Math.floor(t%864e5%36e5%6e4/1e3);return{days:r,hours:c,minutes:l,seconds:m}}
 //# sourceMappingURL=1-timer.js.map
